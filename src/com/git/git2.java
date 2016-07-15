ba388901c1408b6b57b9184070b2b0f360c058f9
ankyzg <624561548@qq.com>
2016-07-15 16:10:12 +0800

    第三次修改 git2

--- src/com/git/git2.java
+++ src/com/git/git2.java
@@ -1,11 +1,9 @@
 package com.git;
-
 public class git2 {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("我是git2");
 		System.out.println("修改第二次");
+		System.out.println("第三次修改");
 	}
-
 }
