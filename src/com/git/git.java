4cc401b122dbe1437f22efbd86cc4b96c1c6a70a
ankyzg <624561548@qq.com>
2016-07-15 14:25:37 +0800

    第三次修改

--- src/com/git/git.java
+++ src/com/git/git.java
@@ -3,9 +3,9 @@ package com.git;
 public class git {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("2222");
 		System.out.println("3333");
+		System.out.println("4444");
+		System.out.println("5555");
 	}
-
 }
