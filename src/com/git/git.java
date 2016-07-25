58dda82b6f2f93f8f19c18d3b8c0458607da1971
ankyzg <624561548@qq.com>
2016-07-25 09:05:03 +0800

    修改内容为66

--- src/com/git/git.java
+++ src/com/git/git.java
@@ -1,11 +1,10 @@
 package com.git;
-
 public class git {
-
 	public static void main(String[] args) {
 		System.out.println("2222");
 		System.out.println("3333");
 		System.out.println("4444");
 		System.out.println("5555");
+		System.out.println("6666");
 	}
 }
